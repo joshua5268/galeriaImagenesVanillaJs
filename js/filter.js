a9a90d862d5dc10d89c4de304410d171dbb3b33a
Joshua Soto <joshua526@hotmail.com>
2022-08-30 17:04:44 -0500

    Nav

--- js/filter.js
+++ js/filter.js
@@ -48,43 +48,19 @@ filtroAb.addEventListener("click", () => {
 
 filtroGender.addEventListener("click", () => {
     if(filtroGender.getAttribute("data-gender") == "male"){
-        let arrayAz = personajes.sort(function (a, b) {
-            if (a.gender == "female" && b.gender == "male") {
-            return 1;
-            }
-            if (b.gender == "female" && a.gender == "male") {
-            return -1;
-            }
-            return 0;
-        });
-        dibujarTarjetas(arrayAz);
+        let arrayFem = personajes.filter(personaje => personaje.gender == "male");
+        dibujarTarjetas(arrayFem);
         document.querySelector("#pGender").innerHTML = "Female";
         filtroGender.setAttribute("data-gender", "female");
     }else{
-        let arrayAz = personajes.sort(function (a, b) {
-            if (b.gender == "female" && a.gender == "male") {
-            return 1;
-            }
-            if (a.gender == "female" && b.gender == "male") {
-            return -1;
-            }
-            return 0;
-        });
-        dibujarTarjetas(arrayAz);
+        let arrayMal = personajes.filter(personaje => personaje.gender == "female");
+        dibujarTarjetas(arrayMal);
         document.querySelector("#pGender").innerHTML = "Male";
         filtroGender.setAttribute("data-gender", "male");
     }
 });
 
 filtroFav.addEventListener("click", () => {
-    let arrayAz = personajes.sort(function (a, b) {
-        if (favoritosStorage.includes(a.id) && !favoritosStorage.includes(b.id)) {
-        return -1;
-        }
-        if (favoritosStorage.includes(b.id) && !favoritosStorage.includes(a.id)) {
-        return 1;
-        }
-        return 0;
-    });
-    dibujarTarjetas(arrayAz);
+    let arrayFav = personajes.filter(personaje => favoritosStorage.includes(personaje.id));
+    dibujarTarjetas(arrayFav);
 });
